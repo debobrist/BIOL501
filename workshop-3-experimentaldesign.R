f8f89a020ca6b930fa4130c58293bfa2129c7f08
Debora Obrist <debora.obrist@live.ca>
2017-09-20 16:38:17 -0700

    Initial commit workshop 3

---
+++ workshop-3-experimentaldesign.R
@@ -0,0 +1,26 @@
+# Random sampling warm-up
+# To begin, get some practice sampling categorical and normally-distributed data from a 
+# population. Consult the “Simulate data” section of the “Plan” tab on the R tips 
+# pages for help.
+
+
+# 1. Sample 20 observations from a population having two groups of individuals, 
+# “infected” and “uninfected”, in equal proportions. 
+# Use a table to show the frequencies of the two categories obtained.
+
+# 2. Repeat the previous step five times to convince yourself that the outcome 
+# varies from sample to sample.
+
+# 3. Sample 18 individuals from a population having two groups of individuals, 
+# “mated” and “unmated”, where the proportion mated in the population is 0.7. 
+# Summarise the frequencies in a table.
+
+# 4. Repeat the previous step five times to convince yourself that the outcome 
+# varies from sample to sample.
+
+# 5. Sample 30 observations from a normally-distributed population having mean 0 
+# and standard deviation 2. Plot the results in a histogram.
+
+# 6. Repeat the following 5 times and calculate the mean each time: sample 30 
+# observations from a normally-distributed population having mean 0 and standard 
+# deviation 2. Convince yourself that the sample mean is different each time.
