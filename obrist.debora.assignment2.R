2751bec6300f8ee7c1da20914d45280b4e7a12bc
Debora Obrist <debora.obrist@live.ca>
2017-11-04 10:21:29 -0700

    adding more things!

--- obrist.debora.assignment2.R
+++ obrist.debora.assignment2.R
@@ -7,11 +7,21 @@
 # Published in American Naturalist, 2012
 sosp <- read.csv ("data-raw/sosp.csv")
 
+# Load required packages: 
+library (lme4)
+library (visreg)
+
 # Look at structure of data:
 str (sosp)
 
 # Change epstatus into a factor:
 sosp$epstatus <- as.factor (sosp$epstatus)
+
+# It makes more sense to me to have a binary factor in the form of  0s and 1s, not 1s and 2s.
+
+# Changing from females = 1, males = 2, to females = 0, males = 1.
+sosp$sex[sosp$sex == 1] <- 0
+sosp$sex[sosp$sex == 2] <- 1
 
 # Visualize data:
 
@@ -30,8 +40,7 @@ stripchart (sosp$LRShatched ~ sosp$epstatus,
 
 axis (1, 
       at = 1:2, 
-      labels = c("within-pair young", "extra-pair young"), 
-      cex.lab = )
+      labels = c("within-pair", "extra-pair"))
 
 # Next, look at LRShatched ~ sex:
 stripchart (sosp$LRShatched ~ sosp$sex,
@@ -48,31 +57,33 @@ axis (1,
       at = 1:2, 
       labels = c("female", "male"))
 
-# Next, look at LRShatched ~ year:
-stripchart (sosp$LRShatched ~ sosp$natalyr,
-            vertical = TRUE,
-            xlab = "year",
-            ylab = "number of offspring hatched",
-            las = 2,
-            pch = 16,
-            method = "jitter",
-            cex.lab = 1.5,
-            cex.axis = 1)
-
-
-# Load required packages: 
-library (lme4)
-library (visreg)
-
 # First, determine what type of linear model is most appropriate. 
+
+# What happens if you just fit a multiple linear regression model? 
+
+sospmod <- lm (sosp$LRShatched ~ sosp$epstatus + sosp$sex)
+
+# Plot all 4 model diagnostic plots at once:
+par (mfrow = c (2, 2))
+plot (sospmod)
+
+# Things don't look great. 
+
+# Let's take a closer look at our parameters: 
 
 # Response: LRShatched
 # Explanatory:
 #   Possible fixed: epstatus, sex
-#   Possible random: year, natal brood, social parent pair
+#   Possible random: natalyr, parentpairID, season, broodcode
+
+# Return to regular plotting pane (1 plot at a time):
+par (mfrow = c (1, 1))
 
 # What is the probability distribution of the response variable?
-hist (sosp$LRShatched)
+hist (sosp$LRShatched,
+      main = "",
+      xlab = "Number of offspring hatched",
+      col = "gray")
 
 # The response variable is not normally distributed, so we have to analyze it with a generalized linear mixed-effects model.
 # It is count data. It is right-skewed, discrete, and bounded between 0 and infinity. That narrows it down to either Poisson
@@ -87,22 +98,57 @@ mean (sosp$LRShatched) # 1.77
 
 # Fit model: 
 
-# sospmod <- glmer (sosp$LRShatched ~ sosp$epstatus + as.factor (sosp$natalyr) + sosp$sex + (1 | sosp$parentpairID) + (1|sosp$broodcode), 
+# sospmod1 <- glmer (sosp$LRShatched ~ sosp$epstatus + sosp$sex + (1 | sosp$parentpairID) + (1|sosp$broodcode) + (1 | as.factor (sosp$natalyr)), 
 #                         family = negative.binomial (link = "log"))
 
 
-# Apparently, you can't use glmer to fit negative binomially distributed data. 
-# I'm going to take it back a step, and for now, ignore the fact that there are both fixed and random effects, 
-# that the variance > the mean, and that the data is probably zero-inflated. I will just fit a simple glm with variables
-# I'm interested in.
+# Apparently, you can't use the glmer function to fit negative binomially distributed data. 
 
-# Fit global model:
-sospmod1 <- glm (LRShatched ~ epstatus + natalyr + sex, 
+# I'm going to take it back a step, and ignore the fact that there is some pseudoreplication, 
+# probably a random effect of year, and that the variance > the mean. 
+# I will just fit a simple glm with explanatory variables I'm interested in - the extra-pair status, 
+# and sex of the bird.
+
+# Fit model with Poisson error distribution: 
+sospmod1 <- glm (LRShatched ~ epstatus + sex, 
                  family = poisson (link = "log"), 
                  data = sosp)
 
-# See parameter estimates:
-coef(sospmod1)
+# Fit model with quasipoisson error distribution to see if data overdispersed:
+sospmod2 <- glm (LRShatched ~ epstatus + sex, 
+                 family = quasipoisson (link = "log"), 
+                 data = sosp)
+
+summary (sospmod2)
+
+# Dispersion parameter for quasipoisson family taken to be 16.51793 - definitely overdispersed!
+
+# See parameter estimates on log-scale:
+coef(sospmod2)
+
+# See back-transformed parameter estimates:
+exp(coef(sospmod2))
+
+
+# What do these parameter estimates mean, biologically? 
+
+# If epstatus = 0, and sex = 0, how many offspring? (this is just intercept)
+# Female, within-pair
+exp(coef (sospmod2)[1] + coef (sospmod2)[2] * 0 + coef (sospmod2)[3] * 0)
+
+# If epstatus = 1, and sex = 0, how many offspring? 
+# Female, extra-pair
+exp(coef (sospmod2)[1] + coef (sospmod2)[2] * 1 + coef (sospmod2)[3] * 0)
+
+# If epstatus = 1, and sex = 1, how many offspring? 
+# Male, extra-pair
+exp(coef (sospmod2)[1] + coef (sospmod2)[2] * 1 + coef (sospmod2)[3] * 1)
+
+# If epstatus = 0, and sex = 1, how many offspring? 
+# Male, within-pair
+exp(coef (sospmod2)[1] + coef (sospmod2)[2] * 0 + coef (sospmod2)[3] * 1)
+
+
 
 # Visualize fit:
 stripchart (sosp$LRShatched ~ sosp$epstatus,
@@ -116,7 +162,7 @@ stripchart (sosp$LRShatched ~ sosp$epstatus,
 axis (1, at = 1:2, labels = c ("within-pair young", "extra-pair young"))
 
 # Use fitted() to generate the predicted values corresponding to data points on the original scale.
-yhat1 <- fitted (sospmod1)
+yhat1 <- fitted (sospmod2)
 
 # Plot linear model: Y = number of offspring hatched values predicted by model, X = extra-pair status.
 lines (yhat1 [order (sosp$epstatus)] ~ sosp$epstatus [order (sosp$epstatus)])
@@ -124,7 +170,7 @@ lines (yhat1 [order (sosp$epstatus)] ~ sosp$epstatus [order (sosp$epstatus)])
 # Add confidence intervals: 
 
 # First, approximate confidence intervals:
-zhat1 <- predict (sospmod1, se.fit = TRUE)       
+zhat1 <- predict (sospmod2, se.fit = TRUE)       
 zupper1 <- zhat1$fit + 1.96 * zhat1$se.fit
 zlower1 <- zhat1$fit - 1.96 * zhat1$se.fit
 
